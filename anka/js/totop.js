8f28b13f56bb77ee1275921032db831852f604ce
Yigit S <55548182+yigitsr@users.noreply.github.com>
2024-02-19 17:23:02 +0300

    Add files via upload

---
+++ anka/js/totop.js
@@ -0,0 +1,19 @@
+const mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 150px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction()
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+   window.location.href = '#'
+}
